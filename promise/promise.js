2df7eb6b5b1e7f6262d531177eb99b23fbf297d8
ChrisGitIt <github.com@l-o-r-e-n-z.com>
2019-07-29 09:12:10 +0200

    Shorten the whole script (DRY)
    I'm not 100% sure, but I think this shorten Version does the same and its a little more obvious what exactly is happening.

--- promise/promise.js
+++ promise/promise.js
@@ -1,21 +1,14 @@
 "use strict"
 
 var PromisePolyfill = require("./polyfill")
-
-if (typeof window !== "undefined") {
-	if (typeof window.Promise === "undefined") {
-		window.Promise = PromisePolyfill
-	} else if (!window.Promise.prototype.finally) {
-		window.Promise.prototype.finally = PromisePolyfill.prototype.finally
+var temp = window ? window : global
+if (typeof temp !== "undefined") {
+	if (typeof temp.Promise === "undefined") {
+		temp.Promise = PromisePolyfill
+	} else if (!temp.Promise.prototype.finally) {
+		temp.Promise.prototype.finally = PromisePolyfill.prototype.finally
 	}
-	module.exports = window.Promise
-} else if (typeof global !== "undefined") {
-	if (typeof global.Promise === "undefined") {
-		global.Promise = PromisePolyfill
-	} else if (!global.Promise.prototype.finally) {
-		global.Promise.prototype.finally = PromisePolyfill.prototype.finally
-	}
-	module.exports = global.Promise
+	module.exports = temp.Promise
 } else {
 	module.exports = PromisePolyfill
 }
